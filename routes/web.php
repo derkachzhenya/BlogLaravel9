dd20d0553f5b9f9a5a853ce432291fbb69895f44
derkachzhenya <zheka4573@gmail.com>
2022-09-16 13:27:27 +0300

    v0.3

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Admin\Main\AdminController;
+use App\Http\Controllers\Admin\Category\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', [IndexController::class, 'index'])->name('index');
 });
+
+Route::group(['namespace'=>'Admin', 'prefix'=>'admin' ], function() {
+
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+    });
+
+    Route::group(['namespace' => 'Category', 'prefix'=>'categories'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('category.index');
+    });
+
+});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
